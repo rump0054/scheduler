b3993c6c803f3e88cfcd271bab0f524f3b178a06
Erik Rumppe <erumppe@gmail.com>
2013-11-20 12:42:26 -0800

    Cleaned up the spec directory - removed all pendings and emptied out carry over specs from previous app

--- spec/factories.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-	factory :user do
-		sequence(:email) { |n| "example#{n}@gmail.com" }
-		password 'blueboy1'
-		first_name 'Example'
-		sequence(:last_name) { |n| "User#{n}" }
-	end
-end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,39 +1,4 @@
 require 'spec_helper'
 
 describe User do
-  before { @user = User.new(email: 'erumppe@gmail.com', password: 'blueboy1', first_name: 'Example', last_name: 'User') }
-
-  subject { @user }
-
-  it { should respond_to(:email) }
-  it { should respond_to(:password) }
-  it { should respond_to(:first_name) }
-  it { should respond_to(:last_name) }
-  it { should respond_to(:user_schedules) }
-
-  it { should be_valid }
-
-  describe "when email is not present" do
-    before { @user.email = '' }
-    it { should_not be_valid }
-  end
-
-  describe "when first_name is not present" do
-    before { @user.first_name = '' }
-    it { should_not be_valid }
-  end
-
-  describe "when last_name is not present" do
-    before { @user.last_name = '' }
-    it { should_not be_valid }
-  end
-
-  describe "when email is already taken" do
-    before do
-      user_with_same_email = @user.dup
-      user_with_same_email.save
-    end
-
-    it { should_not be_valid }
-  end
 end
